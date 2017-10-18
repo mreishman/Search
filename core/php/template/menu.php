ed472579eb3936722591efbf7dcfa3749cdba656
Matt Reishman <matt.reishman@Me.com>
2017-10-18 10:15:17 -0500

    Mostly done with menu

--- core/php/template/menu.php
+++ core/php/template/menu.php
@@ -5,76 +5,131 @@ $URI = $_SERVER['REQUEST_URI'];
 $menuItems = array();
 $count = 0;
 $modifier = "./";
+$imageModifier = "../";
 
 if((strpos($URI, 'main.php') !== false) || (strpos($URI, 'about.php') !== false) || ((strpos($URI, 'whatsNew.php') !== false) || (strpos($URI, 'update.php') !== false) || (strpos($URI, 'changeLog.php') !== false)))
 {
-	$menuItems[$count] = array("title" => "Back" , "action" => "goToUrl('../index.php')");
-	$count++;
+	$menuItems[$count] = array("title" => "Back" , "action" => "goToUrl('../index.php')", "image" => "core/img/backArrow.png");
 }
 else
 {
+	$imageModifier = "./";
 	$modifier = "./settings/";
-	$menuItems[$count] = array("title" => "New Grep" , "action" => "grep()");
-	$count++;
+	$menuItems[$count] = array("title" => "New Grep" , "action" => "grep()", "image" => "core/img/search.png");
 }
+$count++;
 
 if(strpos($URI, 'about.php') !== false)
 {
-	$menuItems[$count] = array("title" => "About" , "action" => "");
-	$count++;
+	$menuItems[$count] = array("title" => "About" , "action" => "", "image" => "");
 }
 else
 {
-	$menuItems[$count] = array("title" => "About" , "action" => "window.location = '".$modifier."about.php'");
-	$count++;
+	$menuItems[$count] = array("title" => "About" , "action" => "window.location = '".$modifier."about.php'", "image" => "");
 }
+$count++;
 
 if(strpos($URI, 'main.php') !== false)
 {
-	$menuItems[$count] = array("title" => "Settings" , "action" => "");
-	$count++;
+	$menuItems[$count] = array("title" => "Settings" , "action" => "", "image" => "core/img/Gear.png");
 }
 else
 {
-	$menuItems[$count] = array("title" => "Settings" , "action" => "window.location = '".$modifier."main.php'");
-	$count++;
+	$menuItems[$count] = array("title" => "Settings" , "action" => "window.location = '".$modifier."main.php'", "image" => "core/img/Gear.png");
 }
+$count++;
 
 if(((strpos($URI, 'whatsNew.php') !== false) || (strpos($URI, 'update.php') !== false) || (strpos($URI, 'changeLog.php') !== false)))
 {
-	$menuItems[$count] = array("title" => "Update" , "action" => "");
-	$count++;
+	$menuItems[$count] = array("title" => "Update" , "action" => "", "image" => "core/img/Refresh.png");
 }
 else
 {
-	$menuItems[$count] = array("title" => "Update" , "action" => "window.location = '".$modifier."update.php'");
+	$menuItems[$count] = array("title" => "Update" , "action" => "window.location = '".$modifier."update.php'", "image" => "core/img/Refresh.png");
+	
+}
+$count++;
+
+//check for Status
+
+if(file_exists('../status/index.php'))
+{
+	$menuItems[$count] = array("title" => "gitStatus" , "action" => "window.location.href =  '../status/';", "image" => "core/img/gitStatus.png");
+	$count++;
+}
+elseif(file_exists('../../status/index.php'))
+{
+	$menuItems[$count] = array("title" => "gitStatus" , "action" => "window.location.href =  '../../status/';", "image" => "core/img/gitStatus.png");
 	$count++;
 }
 
+//check for log-hog
+
+if(file_exists('../Log-Hog/index.php'))
+{
+	$menuItems[$count] = array("title" => "Log-Hog" , "action" => "window.location.href =  '../Log-Hog/';", "image" => "core/img/loghog.png");
+	$count++;
+}
+elseif(file_exists('../../Log-Hog/index.php'))
+{
+	$menuItems[$count] = array("title" => "Log-Hog" , "action" => "window.location.href =  '../../Log-Hog/';", "image" => "core/img/loghog.png");
+	$count++;
+}
+	
+if(file_exists('../loghog/index.php'))
+{
+	$menuItems[$count] = array("title" => "Loghog" , "action" => "window.location.href =  '../loghog/';", "image" => "core/img/loghog.png");
+	$count++;
+}
+elseif(file_exists('../../loghog/index.php'))
+{
+	$menuItems[$count] = array("title" => "Loghog" , "action" => "window.location.href =  '../../loghog/';", "image" => "core/img/loghog.png");
+	$count++;
+}
+	
+
+
 ?>
-<div onclick="toggleMenu();" id="menuMain" style="width: 100%; height: 100%; background-color: rgba(0,0,0,.5); display: none; position: absolute; z-index: 20; padding-top: 65px;">
+<div id="menuMain" style="width: 100%; height: 100%; background-color: rgba(0,0,0,.85); display: none; position: absolute; z-index: 20; -webkit-backdrop-filter: blur(10px); backdrop-filter: blur(10px); overflow-y: scroll;">
+	<div style="border-bottom: 1px dotted grey; height: 64px; z-index: 21; padding: .5em 1em; background-color: black;">
+		<div onclick="toggleMenu();" class="nav-toggle pull-right">
+			<a class="show-sidebar" id="hide">
+		    	<span class="icon-bar"></span>
+		        <span class="icon-bar"></span>
+		        <span class="icon-bar"></span>
+		    </a>
+		</div>
+	</div>
 	<?php
 	foreach ($menuItems as $menuItem => $value): ?>
 		<a style="cursor: pointer" onclick="<?php echo $value['action'];?>">
-		<div style="display: inline-block; width: 150px; height: 150px; margin: 75px; border: 1px solid white;">
-			<?php echo $value['title'];?>
+		<div style="display: inline-block; width: 150px; height: 150px; margin: 75px; z-index: 25;">
+			<img src="<?php echo $imageModifier.$value['image'];?>" style="width: 120px; height: 120px; margin-left: 15px;" >
+			<p style="width: 100%; text-align: center; margin: 0;" ><?php echo $value['title'];?></p>
 		</div>
 		</a>
 	<?php endforeach; ?>
 </div>
-<div style="z-index: 40; border-bottom: 1px dotted grey;" class="backgroundForMenus" id="menu">
-	<table width="100%">
-		<tr>
-			<td width="33%">
-				<a onclick="toggleMenu();" >Menu</a>
-			</td>
-			<td width="34%" style="text-align: center;">
-				
-			</td>
-			<td width="33%">
-			</td>
-		</tr>
-	</table>
+<div style="z-index: 19; border-bottom: 1px dotted grey;" class="backgroundForMenus" id="menu">
+	<div onclick="toggleMenu();" class="nav-toggle pull-right">
+		<a class="show-sidebar" id="show">
+	    	<span class="icon-bar"></span>
+	        <span class="icon-bar"></span>
+	        <span class="icon-bar"></span>
+	    </a>
+	</div>
+	<?php if(strpos($URI, 'main.php') !== false): ?>
+		<a id="mainSettingsMenu2" onclick="goToUrl('#settingsMainVars');" class="active" > Main Settings </a>
+		<a id="watchListSettingsMenu2" onclick="goToUrl('#settingsMainWatch');" > WatchList </a>
+		<a id="menuSettingsMenu2" onclick="goToUrl('#settingsMenuVars');" > Menu Settings </a>
+	<?php endif; ?>
+	<?php if((strpos($URI, 'whatsNew.php') !== false) || (strpos($URI, 'update.php') !== false) || (strpos($URI, 'changeLog.php') !== false)): ?>
+		<a <?php if(strpos($URI, 'update.php') !== false): ?> class='active' <?php else: ?>  onclick="goToUrl('./update.php');"  <?php endif;?> > Update </a>
+		<!-- 
+		<a <?php if(strpos($URI, 'whatsNew.php') !== false): ?> class='active' <?php else: ?>  onclick="goToUrl('./whatsNew.php');"  <?php endif;?> > What's New? </a>
+		-->
+		<a <?php if(strpos($URI, 'changeLog.php') !== false): ?> class='active' <?php else: ?>  onclick="goToUrl('./changeLog.php');"  <?php endif;?> > Changelog </a>
+	<?php endif; ?>
 </div>
 <script type="text/javascript">
 	function toggleMenu()
--- settings/header.php
+++ settings/header.php
@@ -8,22 +8,7 @@ echo loadSentryData($sendCrashInfoJS); ?>
 
 <?php require_once("../core/php/template/menu.php"); ?>
 
-<?php if(strpos($URI, 'main.php') !== false): ?>
-	<div id="menu2">
-		<a id="mainSettingsMenu2" onclick="goToUrl('#settingsMainVars');" class="active" > Main Settings </a>
-		<a id="watchListSettingsMenu2" onclick="goToUrl('#settingsMainWatch');" > WatchList </a>
-		<a id="menuSettingsMenu2" onclick="goToUrl('#settingsMenuVars');" > Menu Settings </a>
-	</div>
-<?php endif; ?>
-<?php if((strpos($URI, 'whatsNew.php') !== false) || (strpos($URI, 'update.php') !== false) || (strpos($URI, 'changeLog.php') !== false)): ?>
-	<div id="menu2">
-		<a <?php if(strpos($URI, 'update.php') !== false): ?> class='active' <?php else: ?>  onclick="goToUrl('./update.php');"  <?php endif;?> > Update </a>
-		<!-- 
-		<a <?php if(strpos($URI, 'whatsNew.php') !== false): ?> class='active' <?php else: ?>  onclick="goToUrl('./whatsNew.php');"  <?php endif;?> > What's New? </a>
-		-->
-		<a <?php if(strpos($URI, 'changeLog.php') !== false): ?> class='active' <?php else: ?>  onclick="goToUrl('./changeLog.php');"  <?php endif;?> > Changelog </a>
-	</div>
-<?php endif; 
+<?php 
 
 $baseUrlImages = "../core/";
 if(file_exists('../local/layout.php'))
