b6fdfa2a2bd19d7aae5c8d703fd67b550848d524
Matt Reishman <matt.reishman@Me.com>
2017-11-07 10:16:32 -0600

    small changes to changelog / config

--- core/php/configStatic.php
+++ core/php/configStatic.php
@@ -1,9 +1,9 @@
 <?php
 
 $configStatic = array(
-  'version'   => '1.0',
+  'version'   => '1.0.1',
   'lastCheck'   => '10-23-2017',
-  'newestVersion' => '1.0',
+  'newestVersion' => '1.0.1',
   'versionList' => array(
   '1.0' => array('branchName' => '1.0','releaseNotes' => '<ul><li>Initial Release</li></ul>')
   )
