320f6243fd9d5e4b0427b93ad243205e5a334b31
Matt Reishman <mreishman@users.noreply.github.com>
2017-11-07 19:46:32 -0600

    Update versionsCheckFile.php

--- versionsCheckFile.php
+++ versionsCheckFile.php
@@ -1,11 +1,15 @@
 <?php
 
 $versionCheckArray = array(
-	'version'		=> '1.0',
+	'version'		=> '1.0.1',
 	'versionList'		=> array(
 		'1.0'	        => array(
 			'branchName'	=> '1.0',
 			'releaseNotes'	=> '<ul><li>Initial Release</li></ul>'
+		),
+		'1.0.1'	        => array(
+			'branchName'	=> '1.0.1',
+			'releaseNotes'	=> '<ul><li>Bug Fixes</li></ul>'
 		),
 	)
 );
